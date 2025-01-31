157a7a5d3330777cc9582eba327374e64ee0cb52
km3ll <kmell@protonmail.com>
2025-01-31 11:50:36 -0500

    Pod 153: Synchronize pod data

--- v2/etl-index-zettels/readme.md
+++ v2/etl-index-zettels/readme.md
@@ -1,1 +1,10 @@
 # etl-index-zettels
+
+## commands
+
+```bash
+mvn clean package
+
+# usage
+java -jar ./target/etl-index-zettels-1.0.jar <folder-path>
+```
--- v2/etl-index-zettels/src/main/java/tsu/pod/indexzettels/Main.java
+++ v2/etl-index-zettels/src/main/java/tsu/pod/indexzettels/Main.java
@@ -4,6 +4,10 @@ import tsu.pod.indexzettels.infra.FileUtils;
 
 public class Main {
     public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java Main <folder-path>");
+            System.exit(1);
+        }
         FileUtils.processFolder(args[0]);
     }
 }
