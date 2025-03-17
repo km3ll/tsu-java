044d1d5fc76f60a5c0ac13dbbd6bc9cd7ac7cd6d
km3ll <kmell@protonmail.com>
2025-03-17 00:18:57 -0500

    Pod 153: Synchronize pod data

---
+++ v2/book-design-patterns/src/main/java/tsu/pod/design_patterns/structural/adapter/adapters/SquarePegAdapter.java
@@ -0,0 +1,23 @@
+package tsu.pod.design_patterns.structural.adapter.adapters;
+
+import tsu.pod.design_patterns.structural.adapter.round.RoundPeg;
+import tsu.pod.design_patterns.structural.adapter.square.SquarePeg;
+
+/**
+ * Adapter allows fitting square pegs into round holes.
+ */
+public class SquarePegAdapter extends RoundPeg {
+    private SquarePeg peg;
+
+    public SquarePegAdapter(SquarePeg peg) {
+        this.peg = peg;
+    }
+
+    @Override
+    public double getRadius() {
+        double result;
+        // Calculate a minimum circle radius, which can fit this peg.
+        result = (Math.sqrt(Math.pow((peg.getWidth() / 2), 2) * 2));
+        return result;
+    }
+}
