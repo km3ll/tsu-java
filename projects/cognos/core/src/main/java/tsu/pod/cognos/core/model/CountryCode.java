8952b9c95cc58bd71513430c5d5efa5ae21657cd
km3ll <kmell@protonmail.com>
2026-01-10 22:42:12 -0500

    Pod 153: Synchronize pod data

--- projects/cognos/core/src/main/java/tsu/pod/cognos/core/model/CountryCode.java
+++ projects/cognos/core/src/main/java/tsu/pod/cognos/core/model/CountryCode.java
@@ -4,30 +4,30 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum CountryCode {
-  CAN("CAN"),
-  USA("USA");
+    CAN("CAN"),
+    USA("USA");
 
-  private final String value;
+    private final String value;
 
-  CountryCode(String value) {
-    this.value = value;
-  }
+    CountryCode(String value) {
+        this.value = value;
+    }
 
-  public String getValue() {
-    return value;
-  }
+    public String getValue() {
+        return value;
+    }
 
-  public static final List<String> CODES =
-      Arrays.stream(values()).map(CountryCode::getValue).sorted().toList();
+    public static final List<String> CODES =
+            Arrays.stream(values()).map(CountryCode::getValue).sorted().toList();
 
-  public static CountryCode from(String value) {
-    return Arrays.stream(values())
-        .filter(code -> code.name().equalsIgnoreCase(value))
-        .findFirst()
-        .orElseThrow(
-            () -> {
-              String msg = "Invalid country code. Expected one of %s. Found %s";
-              return new IllegalArgumentException(String.format(msg, CODES, value));
-            });
-  }
+    public static CountryCode from(String value) {
+        return Arrays.stream(values())
+                .filter(code -> code.name().equalsIgnoreCase(value))
+                .findFirst()
+                .orElseThrow(
+                        () -> {
+                            String msg = "Invalid country code. Expected one of %s. Found %s";
+                            return new IllegalArgumentException(String.format(msg, CODES, value));
+                        });
+    }
 }
